62f4c96175ad54094d516b5ef6c608826019959f
codedysf <132560433+codedysf@users.noreply.github.com>
2023-05-04 12:51:09 +0100

    first commit

---
+++ Addition of numbers/addition/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int num1;
+        int num2;
+        int sum;
+
+
+        System.out.print("Enter your first integer:  ");
+        num1 = input.nextInt();
+
+        System.out.print("Enter your second integer:  ");
+        num2 = input.nextInt();
+
+        sum = num1 + num2;
+
+        System.out.printf("The sum is %d%n", sum);
+
+
+    }
+}

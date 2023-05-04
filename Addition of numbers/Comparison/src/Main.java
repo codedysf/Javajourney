5643f9ed0c81ea42e8a62dc79100b419988d8ba3
codedysf <132560433+codedysf@users.noreply.github.com>
2023-05-04 14:32:28 +0100

    first commit

--- Addition of numbers/Comparison/src/Main.java
+++ Addition of numbers/Comparison/src/Main.java
@@ -10,10 +10,10 @@ public class Main {
         int number2; // second number to compare
 
 
-        System.out.print("Enter first integer "); //prompt
+        System.out.print("Enter first integer"); //prompt
         number1 = input.nextInt(); // read first number from user
 
-        System.out.print("Enter second integer "); // prompt
+        System.out.print("Enter second integer"); // prompt
         number2 = input.nextInt(); // read second number from user
 
         if (number1 == number2)
@@ -29,10 +29,10 @@ public class Main {
             System.out.printf("%d > %d%n", number1, number2);
 
         if (number1 <= number2)
-            System.out.printf("%d <= %d%n", number1, number2);
+            System.out.printf("%d = %d%n", number1, number2);
 
-        if (number1 >= number2)
-            System.out.printf("%d >= %d%n", number1, number2);
+        if (number1 == number2)
+            System.out.printf("%d == %d%n", number1, number2);
 
     }
 }

e9730f2c4709c12fc977271dd33a46d83d99fbc1
codedysf <132560433+codedysf@users.noreply.github.com>
2023-05-04 14:54:46 +0100

    first commit

--- ShapeDisplay/src/Main.java
+++ ShapeDisplay/src/Main.java
@@ -1,45 +1,14 @@
 public class Main {
+    public static void main(String[] args)
+    {
+       drawBox();
 
-    /* This program defines four methods, drawBox(), drawOval(), drawArrow(),
-    and drawDiamond(), each of which prints a specific shape using asterisks.
-    In the main method,these methods are called to display each shape
-     */
-    public static void main(String[] args) {
-        drawBox();
-        drawOval();
-        drawArrow();
-        drawDiamond();
     }
 
-    private static void drawBox() {
+    private static void drawBox(){
         System.out.println("******");
         System.out.println("*    *");
         System.out.println("*    *");
         System.out.println("******");
-    }
-
-    private static void drawOval() {
-        System.out.println("  ***  ");
-        System.out.println(" *   * ");
-        System.out.println("*     *");
-        System.out.println(" *   * ");
-        System.out.println("  ***  ");
-    }
-
-    private static void drawArrow() {
-        System.out.println("    *   ");
-        System.out.println("   ***  ");
-        System.out.println("  ***** ");
-        System.out.println("    *   ");
-        System.out.println("    *   ");
-        System.out.println("    *   ");
-    }
-
-    private static void drawDiamond() {
-        System.out.println("    *   ");
-        System.out.println("   ***  ");
-        System.out.println("  ***** ");
-        System.out.println("   ***  ");
-        System.out.println("    *   ");
     }
 }

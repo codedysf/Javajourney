13180de080e98819cf9c8ff6f7aeb8d1f3f5ef6a
codedysf <132560433+codedysf@users.noreply.github.com>
2023-05-08 11:41:00 +0100

    first commit

---
+++ Account/src/Account.java
@@ -0,0 +1,15 @@
+public class Account {
+
+    public  String name; // instance varialble
+
+
+    // method to set the name in the object
+    public void setName(String name) {
+        this.name = name; // store the name
+    }
+
+    //method to retrieve the name from the object
+    public String getName() {
+        return name; // return value of the name to caller
+    }
+}
---
+++ Account/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+       // create a scanner object to obtain input from the command window
+        Scanner input = new Scanner(System.in);
+
+        //create an object of the class Account and assign it to myAccount
+        Account myAccount  = new Account();
+
+        // to display initial value of name ( null )
+        System.out.println(myAccount.getName());
+
+        //prompt for user to enter name
+        System.out.println("Enter your name : ");
+        String theName = input.nextLine();// read a line of text
+        myAccount.setName(theName);//put theName in myAccount
+
+        // to print the name save in myAccount
+        System.out.println("my name is " + myAccount.getName());
+
+    }
+}

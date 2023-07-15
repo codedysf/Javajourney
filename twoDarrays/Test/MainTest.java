bc43aa157fbaedd265272218e28391b85b19c29c
codedysf <132560433+codedysf@users.noreply.github.com>
2023-07-15 16:19:33 +0100

    first commit

---
+++ twoDarrays/Test/MainTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @org.junit.Test
+    public void main() {
+
+        String username;
+        username = "admin";
+        assertEquals("admin", username);
+    }
+}
